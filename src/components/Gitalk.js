b3f8278fa82b35e1afe4e6bd3f821140273fae60
tristan <yangguochun@ddweilai.com>
2019-09-06 14:00:04 +0800

    feat: gitalk

--- src/components/Gitalk.js
+++ src/components/Gitalk.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import 'gitalk/dist/gitalk.css';
-import GT from 'gitalk';
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import "gitalk/dist/gitalk.css";
+import GT from "gitalk";
+import styled from "styled-components";
 
 const DevWrapper = styled.div`
   margin: 0 auto;
@@ -38,23 +38,23 @@ const GTContainer = styled.div`
   }
 `;
 export default function Gitalk() {
-  const [isProd] = useState(process.env.NODE_ENV == 'production');
+  const [isProd] = useState(process.env.NODE_ENV == "production");
   // const [isProd] = useState(process.env.NODE_ENV == 'development');
   useEffect(() => {
     if (isProd) {
       const language =
-        navigator.language === 'zh-CN' ? navigator.language : 'en';
+        navigator.language === "zh-CN" ? navigator.language : "en";
       const gitalk = new GT({
-        clientID: '9914a43cda9c88e698c5',
-        clientSecret: '68146aa863daf9b7cbbcb59fde7d1514bb6d26da',
-        repo: 'blog.yangerxiao.com',
-        owner: 'zerosoul',
-        admin: ['zerosoul'],
+        clientID: "095fb192736d86fc2d55",
+        clientSecret: "1d437afba62a43c0474c90cd8229617c558ac693",
+        repo: "d2-tech",
+        owner: "d2fe",
+        admin: ["d2fe"],
         language,
         id: location.pathname, // Ensure uniqueness and length less than 50
         distractionFreeMode: false // Facebook-like distraction free mode
       });
-      gitalk.render('gitalk-container');
+      gitalk.render("gitalk-container");
     }
   }, [isProd]);
   return isProd ? (
